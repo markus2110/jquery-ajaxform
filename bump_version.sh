c01e7aa780ac17c95df999e66dc55284017dfed2
Markus (Home) <mail@markus-sommerfeld.de>
2014-07-19 02:09:22 +0200

    added php shell script to find and bump the version count (bump_version.sh 1.2.3)

---
+++ bump_version.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/php
+<?php
+
+if (!isset($argv[1])) {
+    die("Error:\nEnter a new version number!\n\n");
+}
+
+if (file_exists('README.md')) {
+    $file = file_get_contents('README.md');
+    preg_match("/Version\:\s{1,1}.*/", $file, $match);
+    $versionStr = trim($match[0]);
+
+    echo "Found: ".$versionStr."\n";
+
+    $versionNo = explode(" ", $versionStr);
+    $newV = $versionNo[0] . ' ' . $argv[1]. " - ".date('Y-m-d H:i:s');
+    $newFile = str_replace($versionStr, $newV, $file);
+    if (file_put_contents('README.md', $newFile)) {
+        die("Version bumped to " . $argv[1] . "\n");
+    }
+}
